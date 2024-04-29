29dd2210715279c5e6d12005c05e1b84096c7783
Sanyam-jain2304 <103108613+Sanyam-jain2304@users.noreply.github.com>
2024-04-30 04:12:04 +0530

    Create Lonely Integer.py

---
+++ 1 Month Preparation Kit/Week 1/Lonely Integer.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    arr=[]
+    for i in a:
+        if i not in arr:
+            arr.append(i)
+        else:
+            arr.remove(i)
+    return arr[0]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
